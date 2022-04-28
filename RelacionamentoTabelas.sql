20729349388f7f74c27c5802578ed7585d034c08
Daniel Oliveira <93006317+danieloliveiraaa@users.noreply.github.com>
2022-04-28 09:15:55 -0300

    Update RelacionamentoTabelas.sql

--- RelacionamentoTabelas.sql
+++ RelacionamentoTabelas.sql
@@ -1,9 +1,10 @@
-select e.arm_embalagem_numero EMBALAGEM,
-       e.arm_armazem_codigo ARMAZEM_EMB,
-       n.arm_armazem_codigo ARMAZEM_ORIGEM,
-       d.arm_armazem_codigo_transf,
-       d.arm_carregamentodet_dcheckin,
-       d.usu_usuario_codigocheckin
+select e.arm_embalagem_numero          EMBALAGEM,
+       e.arm_armazem_codigo            ARMAZEM_EMB,
+       n.arm_armazem_codigo            ARMAZEM_ORIGEM,
+       d.arm_armazem_codigo_transf     ARMAZEM_TRANSFERIDO,
+       d.arm_carregamentodet_dcheckin  DT_CHECKIN,
+       d.usu_usuario_codigocheckin     USU_CHECKIN,
+       v.con_vfreteconhec_transfchekin
   from tdvadm.t_con_vfreteconhec v,
        tdvadm.t_arm_nota n,
        tdvadm.t_arm_embalagem e,
@@ -15,4 +16,6 @@ select e.arm_embalagem_numero EMBALAGEM,
      and n.arm_embalagem_numero    = e.arm_embalagem_numero
      and e.arm_embalagem_numero    = d.arm_embalagem_numero
      and e.arm_carregamento_codigo = d.arm_carregamento_codigo
-     and e.arm_embalagem_numero    = '1599511'
+     and v.con_valefrete_codigo     = ''
+     and v.glb_rota_codigovalefrete = ''
+     and v.con_valefrete_saque      = ''   
