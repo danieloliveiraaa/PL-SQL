b14ac862786f949adec8f7025348fbe7fcbb8c59
Daniel Oliveira <93006317+danieloliveiraaa@users.noreply.github.com>
2022-05-19 08:00:52 -0300

    Update Desenvolvendoo.sql

--- Desenvolvendoo.sql
+++ Desenvolvendoo.sql
@@ -3,8 +3,8 @@
 --FIFO SE EMBALAGEM ESTIVER SEM CARREGAMENTO
 --DANIEU
 
-CREATE OR REPLACE PROCEDURE SP_EMBALAGEM_SUMIU (P_EMBALAGEM    IN TDVADM.T_ARM_EMBALAGEM.ARM_EMBALAGEM_NUMERO%TYPE,
-                                                --P_CARREGAMENTO IN TDVADM.T_ARM_EMBALAGEM.ARM_CARREGAMENTO_CODIGO%TYPE,
+CREATE OR REPLACE PROCEDURE SP_EMBALAGEM_SUMIU (P_EMBALAGEM    IN TB_EMBALAGEM.EMBALAGEM_NUMERO%TYPE,
+                                                --P_CARREGAMENTO IN TB_EMBALAGEM.CARREGAMENTO_CODIGO%TYPE,
                                                 P_MENSAGEM     OUT VARCHAR2)
 IS   
                                            
@@ -19,8 +19,8 @@ BEGIN
    --embalagem existe??
    SELECT COUNT(*)
      INTO V_COUNT_EMBALAGEM
-     FROM TDVADM.T_ARM_EMBALAGEM E1
-    WHERE TRIM(E1.ARM_EMBALAGEM_NUMERO) = TRIM(P_EMBALAGEM);
+     FROM TB_EMBALAGEM E1
+    WHERE TRIM(E1.EMBALAGEM_NUMERO) = TRIM(P_EMBALAGEM);
    
   
 
@@ -29,26 +29,26 @@ BEGIN
    --verificando se embalagem tem carregamento
    --e se o mesmo existe
    
-   IF(V_COUNT_EMBALAGEM > 0) THEN
+   IF (V_COUNT_EMBALAGEM > 0) THEN
      
-     SELECT E2.ARM_CARREGAMENTO_CODIGO
+     SELECT E2.CARREGAMENTO_CODIGO
        INTO V_CARREGAMENTO
-       FROM TDVADM.T_ARM_EMBALAGEM E2
-      WHERE TRIM(E2.ARM_EMBALAGEM_NUMERO) = TRIM(P_EMBALAGEM);
+       FROM TB_EMBALAGEM E2
+      WHERE TRIM(E2.EMBALAGEM_NUMERO) = TRIM(P_EMBALAGEM);
   
      
-     --se embalagem estiver sem carregamento, logo deverá aparecer no fifo
+     --se embalagem estiver sem carregamento, logo deverÃ¡ aparecer no fifo
      --para ser vinculado a um.
           
      IF (V_CARREGAMENTO IS NULL) THEN
        
         --verificando se embalagem tem contrato,
-        --se não tem, não aparece no fifo.
+        --se nÃ£o tem, nÃ£o aparece no fifo.
                                 
         SELECT N.SLF_CONTRATO_CODIGO
           INTO V_EXISTE_CONTRATO
-          FROM TDVADM.T_ARM_NOTA N
-         WHERE TRIM(N.ARM_EMBALAGEM_NUMERO) = TRIM(N.ARM_EMBALAGEM_NUMERO);
+          FROM TB_NOTA N
+         WHERE TRIM(N.EMBALAGEM_NUMERO) = TRIM(N.EMBALAGEM_NUMERO);
         
         IF(V_EXISTE_CONTRATO IS NULL) THEN
         
@@ -56,16 +56,16 @@ BEGIN
         
         ELSE
           
-          --atualizo a data inclusão e fechado para uma data atual.
+          --atualizo a data inclusÃ£o e fechado para uma data atual.
           
           UPDATE TDVADM.T_ARM_EMBALAGEM E3
-             SET E3.ARM_EMBALAGEM_DTINCLUSAO   = SYSDATE
-           WHERE TRIM(E3.ARM_EMBALAGEM_NUMERO) = TRIM(P_EMBALAGEM)
+             SET E3.EMBALAGEM_DTINCLUSAO   = SYSDATE
+           WHERE TRIM(E3.EMBALAGEM_NUMERO) = TRIM(P_EMBALAGEM)
           COMMIT;
               
-          UPDATE TDVADM.T_ARM_EMBALAGEM E4
-             SET E4.ARM_EMBALAGEM_DTFECHADO    = SYSDATE
-           WHERE TRIM(E4.ARM_EMBALAGEM_NUMERO) = TRIM(P_EMBALAGEM)
+          UPDATE TB_EMBALAGEM E4
+             SET E4.EMBALAGEM_DTFECHADO    = SYSDATE
+           WHERE TRIM(E4.EMBALAGEM_NUMERO) = TRIM(P_EMBALAGEM)
            COMMIT;
         
           P_MENSAGEM := 'Embalagem no FIFO, verificar!';  
@@ -79,9 +79,9 @@ BEGIN
        --verificando se carregamento tem data de fechamento.
        SELECT COUNT(*)
          INTO V_FECHADO
-         FROM TDVADM.T_ARM_CARREGAMENTO C
-        WHERE TRIM(C.ARM_CARREGAMENTO_CODIGO) = TRIM(V_CARREGAMENTO)
-          AND C.ARM_CARREGAMENTO_DTFECHAMENTO IS NOT NULL; --preenchido após fechar
+         FROM TB_CARREGAMENTO C
+        WHERE TRIM(C.CARREGAMENTO_CODIGO) = TRIM(V_CARREGAMENTO)
+          AND C.CARREGAMENTO_DTFECHAMENTO IS NOT NULL; --preenchido apÃ³s fechar
           
           IF(V_FECHADO > 0) THEN
             
@@ -90,33 +90,33 @@ BEGIN
             --atualizo embalagem e a data de fechamento
             --do carregamento para data atual.
             
-            UPDATE TDVADM.T_ARM_EMBALAGEM E5
-               SET E5.ARM_EMBALAGEM_DTINCLUSAO   = SYSDATE
-             WHERE TRIM(E5.ARM_EMBALAGEM_NUMERO) = TRIM(P_EMBALAGEM)
+            UPDATE TDVADM.TB_EMBALAGEM E5
+               SET E5.EMBALAGEM_DTINCLUSAO   = SYSDATE
+             WHERE TRIM(E5.EMBALAGEM_NUMERO) = TRIM(P_EMBALAGEM)
              COMMIT;
               
-            UPDATE TDVADM.T_ARM_EMBALAGEM E6
-               SET E6.ARM_EMBALAGEM_DTFECHADO    = SYSDATE
-             WHERE TRIM(E6.ARM_EMBALAGEM_NUMERO) = TRIM(P_EMBALAGEM)
+            UPDATE TB_EMBALAGEM E6
+               SET E6.EMBALAGEM_DTFECHADO    = SYSDATE
+             WHERE TRIM(E6.EMBALAGEM_NUMERO) = TRIM(P_EMBALAGEM)
              COMMIT;
              
-            UPDATE TDVADM.T_ARM_CARREGAMENTO C1
-               SET C1.ARM_CARREGAMENTO_DTFECHAMENTO = SYSDATE
-             WHERE TRIM(C1.ARM_CARREGAMENTO_CODIGO) = TRIM(V_CARREGAMENTO)
+            UPDATE TB_CARREGAMENTO C1
+               SET C1.CARREGAMENTO_DTFECHAMENTO = SYSDATE
+             WHERE TRIM(C1.CARREGAMENTO_CODIGO) = TRIM(V_CARREGAMENTO)
              COMMIT;
              
              
             --buscando veiculo disp de um outro carregamento
-            --obs: do mesmo armazém 
+            --obs: do mesmo armazÃ©m 
             --veiculo disp sem placa
             
             SELECT D.CAR_VEICULO_PLACA
               INTO V_VEICULODISP
-              FROM TDVADM.T_FCF_VEICULODISP  D,
-                   TDVADM.T_ARM_CARREGAMENTO C
-             WHERE C.ARM_CARREGAMENTO_CODIGO = D.ARM_CARREGAMENTO_CODIGO
-               AND C.FCF_VEICULODISP_CODIGO  = D.FCF_VEICULODISP_CODIGO
-               AND C.ARM_CARREGAMENTO_CODIGO = V_CARREGAMENTO;
+              FROM TB VEICULODISP  D,
+                   TB_CARREGAMENTO C
+             WHERE C.CARREGAMENTO_CODIGO = D.CARREGAMENTO_CODIGO
+               AND C.VEICULODISP_CODIGO  = D.VEICULODISP_CODIGO
+               AND C.CARREGAMENTO_CODIGO = V_CARREGAMENTO;
                
                
                IF() THEN
@@ -155,28 +155,5 @@ BEGIN
 
    
    
-      
-   --SE O COUNT ACIMA DER ACIMA DE 0
-   --EXECUTA O UPDATE
-   IF (V_LOGIN_EXISTE > 0) AND (V_SENHA > 0) THEN
-       
-     UPDATE WTDVCORE.T_PES_USUARIO U3
-     SET U3.PES_USUARIO_SENHA = '55de58ef9a8b1d07bff4b02aa702c3fe06ff27f2731ba46409c2dcf183489734'
-     WHERE U3.PES_USUARIO_LOGIN = p_LOGIN;
-     p_MENSAGEM := 'SENHA DO USUÁRIO REDEFINIDA.';
-     commit;
-   
-   --VERIFICA SE O USUÁRIO EXISTE
-   ELSIF (V_LOGIN_EXISTE = 0) THEN
      
-     p_MENSAGEM := 'O USUÁRIO DIGITADO NÃO EXISTE!';
-   
-   --VERIFICA SE A SENHA É AMARELO1  
-   ELSE --(V_SENHA < 0) THEN
-     
-     p_MENSAGEM := 'A SENHA DESTE USUÁRIO JA É amarelo1 ';
-     
-   END IF;
-   
-     
-END SP_REDEFINIR_SENHA_EXTRANET;
+END;
